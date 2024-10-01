bd385b8b37c647dda03d5d8786f56f00571d00ce
flavinha07 <132069038+flavinha07@users.noreply.github.com>
2024-09-30 22:01:09 -0300

    Add files via upload

--- asadelta/Scenes/bird.gd
+++ asadelta/Scenes/bird.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 class_name Bird
 
+signal game_started
+
 @export var gravity = 900.0
 @export var jump_force = -300
 @export var rotation_speed = 2 
@@ -19,6 +21,7 @@ func _ready():
 func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") && should_process_input:
 		if !is_started:
+			game_started.emit()
 			animation_player.play("flap_wings")
 			is_started = true
 		jump()
@@ -46,6 +49,9 @@ func rotate_bird():
 	# rotate upwards when rising
 	elif velocity.y < 0 && rad_to_deg(rotation) > -30:
 		rotation -= rotation_speed * deg_to_rad(1)
+
+func kill():
+	should_process_input = false 
 
 func stop():
 	animation_player.stop()
